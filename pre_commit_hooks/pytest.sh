da4a9bc92e56ee15cea34e69a2b05bb381e07ebd
Mathis Antony <sveitser@gmail.com>
2018-03-04 22:24:19 +0800

    misc fixes
    - move hooks.yaml to .pre-commit-hooks.yaml
    - call pytest directly
    - fix dummy package name
    - add pytest-cov dependency
    - remove shell script

--- pre_commit_hooks/pytest.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-args=("$@")
-git stash -q --keep-index
-py.test $args
-RESULT=$?
-git stash pop -q
-[ $RESULT -ne 0 ] && exit 1
-exit 0
--- setup.py
+++ setup.py
@@ -2,7 +2,10 @@ from setuptools import setup
 
 
 setup(
-    name='__dummy_package',
+    name='pre_commit_dummy_package',
     version='0.0.0',
-    install_requires=['pytest>=2.0.0'],
+    install_requires=[
+        'pytest>=2.0.0',
+        'pytest-cov',
+    ],
 )
